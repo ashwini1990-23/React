306f17dffaed32c1582789a88ac3e3b725b7fefd
Ashwini <ashwiniaski23@gmail.com>
2025-11-20 15:49:29 -0500

    Added files needed for project and started working on pizza-menu project

--- 03-Pizza-Menu/pizza-menu/src/main.jsx
+++ 03-Pizza-Menu/pizza-menu/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// Render our app in the DOM in React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// React before v18
+// ReactDOM.render(<App/>, document.getElementById("root"))
